e040f1d16cdcfd2c60d57dbb92692fe90e208447
Chris Caspanello <ccaspanello@pentaho.com>
2018-05-10 22:48:44 -0400

    [WIP] Plugin system in place but with classloading issues.

--- beam-app/src/main/java/org/pentaho/beam/app/TransformationMain.java
+++ beam-app/src/main/java/org/pentaho/beam/app/TransformationMain.java
@@ -2,37 +2,36 @@ package org.pentaho.beam.app;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.pentaho.beam.app.transformation.StepRegistry;
-import org.pentaho.beam.app.transformation.TransContext;
-import org.pentaho.beam.app.transformation.TransformationOptions;
-import org.pentaho.beam.app.transformation.api.TransMeta;
-import org.pentaho.beam.app.transformation.api.Transformation;
-import org.pentaho.beam.app.transformation.gson.TransMetaConverter;
+import org.pentaho.beam.app.api.gson.TransMetaConverter;
+import org.pentaho.beam.app.api.plugin.StepRegistry;
+import org.pentaho.beam.app.api.trans.Transformation;
+import org.pentaho.beam.app.api.transmeta.TransContext;
+import org.pentaho.beam.app.api.transmeta.TransMeta;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
-/**
- * TODO Replace tutorial code with ETL program
- */
 public class TransformationMain {
 
   private static final Logger LOG = LoggerFactory.getLogger( TransformationMain.class );
 
   public static void main( String[] args ) {
+
+
     TransformationOptions options = PipelineOptionsFactory
       .fromArgs( args )
       .withValidation()
       .as( TransformationOptions.class );
     Pipeline p = Pipeline.create( options );
 
-    String transformationFile = options.getTransformationFile();
-    TransMetaConverter converter = new TransMetaConverter();
-    TransMeta transMeta = converter.fromJson( new File(transformationFile), options.getParameters() );
     StepRegistry stepRegistry = new StepRegistry();
     stepRegistry.init();
     TransContext transContext = new TransContext( p, stepRegistry );
+
+    String transformationFile = options.getTransformationFile();
+    TransMetaConverter converter = new TransMetaConverter(stepRegistry);
+    TransMeta transMeta = converter.fromJson( new File( transformationFile ), options.getParameters() );
     Transformation transformation = new Transformation( transContext, transMeta );
     transformation.execute();
 
@@ -43,4 +42,6 @@ public class TransformationMain {
 
     p.run().waitUntilFinish();
   }
+
+
 }
